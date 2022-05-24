c2d2a7b7f1f1adf744702b7f1171f2e4f651a762
Administrator <Administrator@WINDOWS-BVQNF6J>
2022-05-24 16:09:57 +0530

    Add project files.

---
+++ CSharpConcepts/Class12.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConcepts
+{
+    internal class Class12
+    {
+        static void Main(string[] args)
+        {
+            static void Main(String[] args)
+            {
+                if (m <= 20)
+                {
+                    Console.WriteLine("A");
+                }
+                else if (m >= 10)
+                {
+                    Console.WriteLine("B");
+                }
+                else (m >= 50){
+                    Console.WriteLine("C");
+                }
+            }
+
+        }
+
+    }
+}
